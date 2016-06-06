224bc71c83457f1f63459d83606aa1928dffe46c
KentaMasago <masagotty@gmail.com>
2016-06-06 14:28:16 +0900

    check

--- app/src/main/java/android/lifeistech/com/count/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/count/MainActivity.java
@@ -7,21 +7,50 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    int count;
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    int count1;
+    int count2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        count = 0;
+        text1 = (TextView)findViewById(R.id.text1);
+        text2 = (TextView)findViewById(R.id.text2);
+        text3 = (TextView)findViewById(R.id.text3);
+        count1 = 0;
+        count2 = 0;
     }
 
-    public void add(View v){
-        count = count + 1;
+    public void plus1(View v){
+        count1 ++;
 
-        textView.setText(count + "回ボタンが押されました！");
+        text1.setText(count1 + "回ボタンが押されました！");
     }
+
+    public void plus2(View v){
+        count2 ++;
+
+        text2.setText(count2 + "回ボタンが押されました！");
+    }
+
+    public void subtract1(View v){
+        count1 --;
+
+        text1.setText(count1 + "回ボタンが押されました！");
+    }
+
+    public void subtract2(View v){
+        count2 --;
+
+        text2.setText(count2 + "回ボタンが押されました！");
+    }
+
+    public void sum(View v){
+        text3.setText("合計は" + (count1 + count2)  + "です！");
+    }
+    
 }
